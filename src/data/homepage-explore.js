9f0fc33f26dfc47cdc7c0cb97efa06de67cf79d5
GAUTAM JAIN <g9213733800@gmail.com>
2024-04-01 15:06:56 +0530

    first commit

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Learn HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive Web design",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive ",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Java",
+                description : "This course covers the basic concepts of Java such as syntax, data types, control structures, object-oriented programming principles, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Python",
+                description : "This course covers Python basics, including data types, control flow, functions, and advanced topics like OOP and file handling.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "SCSS",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Flask",
+                description : "This course introduces Flask, a lightweight web application framework in Python, covering topics such as routing, templates, forms, database integration, and RESTful APIs.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Django",
+                description : "This course explores Django, a high-level Python web framework, focusing on topics such as MVC architecture, ORM, templating, forms, authentication, and building RESTful APIs.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Fast API",
+                description : "This course covers FastAPI, a Python framework for building APIs with speed, async capabilities, automatic documentation, input validation, and dependency injection.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Next.js",
+                description : "This course explores Next.js, a React framework for building server-side rendered and statically generated web applications, covering features like routing, data fetching, server-side rendering, and API routes.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Nuxt.js",
+                description : "This course covers Nuxt.js, a Vue.js framework for building server-side rendered and statically generated web applications, focusing on features such as routing, data fetching, server-side rendering, and module integration.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Sanity",
+                description : "This course covers Sanity, a flexible headless CMS emphasizing real-time collaboration and structured content delivery via APIs.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+]
